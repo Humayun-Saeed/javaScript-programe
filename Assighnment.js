b065351452f24123444d8f08d8186c741f9b285c
Humayun-Saeed <95467873+Humayun-Saeed@users.noreply.github.com>
2021-12-30 00:35:45 -0800

    Assighnment

---
+++ Assighnment.js
@@ -0,0 +1,20 @@
+/*There are diffrent kind of assighnment opreatrs are mention below:
+=	x = y	x = y
++=	x += y	x = x + y
+-=	x -= y	x = x - y
+*=	x *= y	x = x * y
+/=	x /= y	x = x / y
+%=	x %= y	x = x % y
+<<=	x <<= y	x = x << y
+>>=	x >>= y	x = x >> y
+>>>=	x >>>= y	x = x >>> y
+&=	x &= y	x = x & y
+^=	x ^= y	x = x ^ y
+|=	x |= y	x = x | y
+**=	x **= y	x = x ** y
+
+*/
+//The += assignment operator adds a value to a variable.
+let x=5;
+x+=5;
+console.log(x);
