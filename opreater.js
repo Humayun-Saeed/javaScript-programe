94c0d5cd265e45f12976672f19cb5e4629f25c6a
Humayun-Saeed <95467873+Humayun-Saeed@users.noreply.github.com>
2021-12-30 00:31:25 -0800

    opreater

---
+++ opreater.js
@@ -0,0 +1,20 @@
+/*There are diffrent kind of opreatrs in javascript
++,-,*,/,%, Basically there are two kind of opreaters
+Arithmetic opreaters
+Assighnemnt opreaters*/
+
+//The assignment operator (=) assigns a value to a variable.
+let x = 10;
+
+//The addition operator (+) adds numbers:
+
+let x = 5;
+let y = 2;
+let z = x + y;
+console.log(z);
+
+//The multiplication operator (*) multiplies numbers
+let x = 5;
+let y = 2;
+let z = x * y;
+console.log(z);
