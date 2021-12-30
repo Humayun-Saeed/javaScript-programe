97f9027c8d56153bfef43cb10247107043420ae7
Humayun-Saeed <95467873+Humayun-Saeed@users.noreply.github.com>
2021-12-30 00:31:52 -0800

    arithmetic

---
+++ Arithmetic.js
@@ -0,0 +1,37 @@
+/*Arithmetic operators perform arithmetic on numbers (literals or variables)
+
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Remainder)
+++	Increment
+--	Decrement 
+*/
+
+//The modulus operator (%) returns the division remainder.
+let x = 5;
+let y = 2;
+let z = x % y;
+console.log(z);
+
+//The increment operator (++) increments numbers.
+
+let x = 5;
+x++;
+let z = x;
+console.log(z);
+
+//The decrement operator (--) decrements numbers.
+
+let x = 5;
+x--;
+let z = x;
+console.log(z);
+
+//The exponentiation operator (**) raises the first operand to the power of the second operand.
+
+let x=5;
+let z=(x**2);
+console.log(z);
