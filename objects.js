3527de0d29bae34b5f71e015ec31b162c607090c
Humayun-Saeed <95467873+Humayun-Saeed@users.noreply.github.com>
2021-12-30 00:35:21 -0800

    objects

---
+++ objects.js
@@ -0,0 +1,23 @@
+/* As we know variables are used to store the values . But object is also a variable
+but you can add diffrent kind of data in a object like attributes e.g a car is a object
+you can add the attributes like color type etc.
+
+
+*/
+const car = {type:"Fiat", model:"500", color:"white"};
+
+console.log(car.type);
+
+const  person{
+
+    name:"ali";
+    Age:10;
+    height:5;
+    work:function work(){
+console.log("hello world");
+
+    }
+
+};
+
+console.log(work());
